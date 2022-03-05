7d5c923c2583d7661b709138e6d60b105b7260b9
jcagua <Jonathan.cagua@easymetering.com>
2022-03-05 16:07:02 -0500

    FIX: ex7

--- Class/ex7.py
+++ Class/ex7.py
@@ -1,8 +1,13 @@
-with open('file.txt') as archivo:
-    lineas = archivo.readlines()
-    my_list = []
-    for element in lineas:
-        element=element.strip("valor=")
-        element=element.strip("\n") 
-        my_list.append(element)
-    print(my_list)
+
+def leer(arg1):
+    with open(arg1) as archivo:
+        lineas = archivo.readlines()
+        my_list = []
+        for element in lineas:
+            element=element.strip("valor=")
+            element=element.strip("\n") 
+            my_list.append(element)
+        return(my_list)
+
+ret = leer('file.txt')
+print(ret)
