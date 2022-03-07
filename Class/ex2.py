8495480d68c62cf4692d41fb7d0a9ac4112a5fbc
jcagua <Jonathan.cagua@easymetering.com>
2022-03-07 18:20:14 -0500

    FIX: for extra por eso fue eliminado

--- Class/ex2.py
+++ Class/ex2.py
@@ -4,13 +4,11 @@ print(id(my_list))
 print(type(my_list))
 print(len(my_list))
 
-for element in my_list:
-    print (element)
+print (my_list)
 
 my_list.append(10)
 print(id(my_list))
 print(type(my_list))
 print(len(my_list))
 
-for element in my_list:
-    print (element)
+print (my_list)
