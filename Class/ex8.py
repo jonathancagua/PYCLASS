3c1ad2521a47ec04e1e0076af15f59c849ba7c31
jcagua <Jonathan.cagua@easymetering.com>
2022-03-11 13:40:24 -0500

    FIX: name

--- Class/ex8.py
+++ Class/ex8.py
@@ -1,10 +1,10 @@
 
-def leer(arg):
+def escribir(arg):
     out = open('file.txt', 'w')
     for k, v in arg.items():
         out.writelines(k +"="+v+"\n")
     out.close()
 
 dic = {'ip': '192.168.0.1', 'nombre': 'Juan', 'estado': 'ON', 'color': '#555555', 'log': '3M'}
-ret = leer(dic)
+ret = escribir(dic)
 
