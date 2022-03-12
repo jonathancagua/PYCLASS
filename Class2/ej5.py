11d2045d07828c45f7674173dcfb8c472b66abb9
jcagua <Jonathan.cagua@easymetering.com>
2022-03-12 11:26:48 -0500

    FIX: ejercicio5

--- Class2/ej5.py
+++ Class2/ej5.py
@@ -2,7 +2,7 @@ from personas import Persona
 
 p= Persona("jon",39)
 p2 = Persona("Clara",55)
-presp = p.get_mayor(p,p2)
+presp = Persona.get_mayor(p,p2)
 print("Es mayor:")
 presp.print_persona()
 
