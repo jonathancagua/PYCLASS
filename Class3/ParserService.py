8e460c0388a68a7a21db98adff4944917af0f6ec
Jonathan Cagua <jonathan.cagua@gmail.com>
2022-03-20 20:26:17 -0500

    ADD: change

--- Class3/ParserService.py
+++ Class3/ParserService.py
@@ -3,9 +3,25 @@ import sys
 from inventario import inventario
 import json
 import threading
+import signal  
+import time
+import traceback
+cerrado=False
+global timer
 
+def handler(sig, frame):  # define the handler  
+        print('You pressed Ctrl+C!')
+        global cerrado
+        traceback.print_stack(frame)
+        #sys.exit(0)
+        cerrado = True
+        #sys.exit(0)
+        
 def func():
     try:
+        global cerrado
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        server_address = ('localhost', 4000)
         lista = p1.load_csv()
         dataAtx = json.dumps(lista)
 
@@ -13,18 +29,22 @@ def func():
         print('waiting to receive')
         data, server = sock.recvfrom(4096)
         print('received {!r}'.format(data))
-        global timer
-        timer = threading.Timer(3, func)
-        timer.start()
+       
+        if cerrado == False:
+            timer = threading.Timer(3, func)
+            timer.start()
+        
     finally:
         print('closing socket')
-        #sock.close()
+        sock.close()
+        if cerrado == True:
+            sys.exit(0)
 
 #carga el directorio donde se va leer
+cerrado = False
 p1= inventario()
 # Create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('localhost', 4000)
+signal.signal(signal.SIGINT, handler)
 func()
 
 
