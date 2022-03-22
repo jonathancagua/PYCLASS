5ded981bb75a74a7db9891e654b4cb82774f1a60
jcagua <Jonathan.cagua@easymetering.com>
2022-03-22 10:24:40 -0500

    FIX: change the bucle

--- Class3/ParserService.py
+++ Class3/ParserService.py
@@ -2,7 +2,7 @@ import socket
 import sys
 from inventario import Inventario
 import json
-import threading
+#import threading
 import signal  
 import time
 import traceback
@@ -10,38 +10,32 @@ cerrado=False
 global timer
 
 def handler(sig, frame):  # define the handler  
-        print('You pressed Ctrl+C!')
         global cerrado
+        print('You pressed Ctrl+C!')
         traceback.print_stack(frame)
-        #sys.exit(0)
-        cerrado = True
-        #sys.exit(0)
+        cerrado = False
         
 def main():
-    try:
-        global cerrado
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        server_address = ('localhost', 4000)
-        lista = p1.load_csv()
-        dataAtx = json.dumps(lista)
+    global cerrado
+    while cerrado == True:
+        try: 
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            server_address = ('localhost', 4000)
+            lista = p1.load_csv()
+            dataAtx = json.dumps(lista)
 
-        sent = sock.sendto(dataAtx.encode(encoding="utf-8"), server_address)
-        print('waiting to receive')
-        data, server = sock.recvfrom(4096)
-        print('received {!r}'.format(data))
-       
-        if cerrado == False:
-            timer = threading.Timer(3, func)
-            timer.start()
-        
-    finally:
-        print('closing socket')
-        sock.close()
-        if cerrado == True:
-            sys.exit(0)
+            sent = sock.sendto(dataAtx.encode(encoding="utf-8"), server_address)
+            print('waiting to receive')
+            data, server = sock.recvfrom(4096)
+            print('received {!r}'.format(data))
+        finally:
+            print('closing socket')
+            sock.close()
+        time.sleep(3)
+
 
 #carga el directorio donde se va leer
-cerrado = False
+cerrado = True
 p1= Inventario()
 # Create a UDP socket
 signal.signal(signal.SIGINT, handler)
