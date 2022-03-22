09a5d45eac48055bac1e36c1a6d5f76d61410874
jcagua <Jonathan.cagua@easymetering.com>
2022-03-22 11:41:30 -0500

    FIX: close process

--- Class3/ParserService.py
+++ Class3/ParserService.py
@@ -2,43 +2,52 @@ import socket
 import sys
 from inventario import Inventario
 import json
-#import threading
 import signal  
 import time
 import traceback
-cerrado=False
-global timer
 
-def handler(sig, frame):  # define the handler  
-        global cerrado
+class ServiceExit(Exception):
+    """
+    Custom exception which is used to trigger the clean exit
+    of all running threads and the main program.
+    """
+    print('Saliendo')
+    pass
+
+
+class Main:     
+    def handler(self, sig, frame):  # define the handler  
         print('You pressed Ctrl+C!')
-        traceback.print_stack(frame)
-        cerrado = False
-        
-def main():
-    global cerrado
-    while cerrado == True:
-        try: 
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            server_address = ('localhost', 4000)
-            lista = p1.load_csv()
-            dataAtx = json.dumps(lista)
+        raise ServiceExit()
 
-            sent = sock.sendto(dataAtx.encode(encoding="utf-8"), server_address)
-            print('waiting to receive')
-            data, server = sock.recvfrom(4096)
-            print('received {!r}'.format(data))
-        finally:
-            print('closing socket')
-            sock.close()
-        time.sleep(3)
+    def main(self):
+        signal.signal(signal.SIGINT, self.handler)
+        while True:
+            try: 
+                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                server_address = ('localhost', 4000)
+                lista = p1.load_csv()
+                dataAtx = json.dumps(lista)
+                sent = sock.sendto(dataAtx.encode(encoding="utf-8"), server_address)
+                print('waiting to receive')
+                data, server = sock.recvfrom(4096)
+                print('received {!r}'.format(data))
+                print('closing socket')
+                sock.close()
+                time.sleep(3)
+            except ServiceExit:
+                print("ServiceExit")
+                exit() 
+	            
+            
+
+
 
 
 #carga el directorio donde se va leer
-cerrado = True
 p1= Inventario()
-# Create a UDP socket
-signal.signal(signal.SIGINT, handler)
-main()
+# Create main service
+m = Main()
+m.main()
 
 
